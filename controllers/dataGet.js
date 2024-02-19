9e1059acf0d978858e451f987ac877014d4d77b9
Karan1002 <karankushwaha7227@gmail.com>
2024-02-20 01:04:13 +0530

    3rd

--- controllers/dataGet.js
+++ controllers/dataGet.js
@@ -2,14 +2,18 @@ const animalModel = require("../model/animalModel");
 const mysqlDBConnect = require("../config/mysqlDBConnect");
 
 const getAllData = async (req, res) => {
-  //   let data = await mongoDataGet(req.body.id);
-  let dataMysql = mysqlDataGet(req.body.id);
-  console.log("dsdsdcsd" + dataMysql);
+  const dataMongo = await mongoDataGet(req.body.id);
+  const dataMysql = await mysqlDataGet(req.body.id);
+  const data = {
+    dataMongo,
+    dataMysql,
+  };
+  console.log(data);
 };
 
 const mongoDataGet = async (id) => {
   try {
-    const getData = await animalModel.findOne({ a_id: id }).exec();
+    const getData = await animalModel.findOne({ a_id: id });
     if (!getData) {
       return console.log("not data found");
     }
@@ -19,25 +23,20 @@ const mongoDataGet = async (id) => {
   }
 };
 
-const mysqlDataGet = (id) => {
-  try {
-    mysqlDBConnect.query(
-      `SELECT * FROM animals WHERE a_id='${id}'`,
-      (error, results, fields) => {
-        if (error) throw error;
-        return results.map(function (row) {
-          return {
-            a_id: row.a_id,
-            animal_name: row.animal_name,
-            description: row.description,
-            location: row.location,
-          };
-        });
-        // return results;
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
+const mysqlDataGet = async (id) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const dboutput = mysqlDBConnect.query(
+        `SELECT * FROM animals WHERE a_id='${id}'`,
+        async (error, results, fields) => {
+          if (error) throw error;
+          resolve(results);
+        }
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
+
 module.exports = getAllData;
